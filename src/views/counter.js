2e75788d0c706974d1787e783dddb2f5564d1d8b
Tom Zellman <tzellman@gmail.com>
2017-02-12 17:53:48 -0500

    Split out a button component

--- src/views/counter.js
+++ src/views/counter.js
@@ -6,26 +6,20 @@ const clickCount = clicks => {
     return clicks > 0 ? html`<div>You clicked ${clicks} time${clicks > 1 ? 's' : ''}</div>` : '';
 };
 
+const Button = ({className, label, update, disabled}) => (html`
+    <button class="${className}" onclick=${update} disabled=${disabled ? disabled : false}>
+        ${label}
+    </button>
+`);
 
-const view = (model, msg) => (html`
+const view = (model, actions) => (html`
   <div class="counter">
     <h1>Welcome to HyperApp!</h1>
     <hr class="line-break" />
     <section>
-      <button
-        class="add"
-        onclick=${msg.add}
-      >
-        +
-      </button>
+      ${Button({className: 'add', label: '+', update: actions.add})}
       <h1>${model.num}</h1>
-      <button
-        class="sub"
-        onclick=${msg.sub}
-        disabled=${model.num <= 0}
-      >
-        -
-      </button>
+      ${Button({className: 'sub', label: '-', update: actions.sub, disabled: model.num <= 0})}
       ${clickCount(model.clicks)}
     </section>
   </div>
