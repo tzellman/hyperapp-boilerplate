0a52a1d3538f7d68520cef58c7c50c3215d78d6f
Tom Zellman <tzellman@gmail.com>
2017-02-22 01:10:20 -0500

    No more hyperx - using JSX

--- src/views/counter.js
+++ src/views/counter.js
@@ -1,28 +1,26 @@
 import {h} from 'hyperapp';
-import hyperx from 'hyperx';
-const html = hyperx(h);
 
 const clickCount = clicks => {
-    return clicks > 0 ? html`<div>You clicked ${clicks} time${clicks > 1 ? 's' : ''}</div>` : '';
+    return clicks > 0 ? <div>You clicked {clicks} time{clicks > 1 ? 's' : ''}</div> : '';
 };
 
-const Button = ({className, label, update, disabled}) => (html`
-    <button class="${className}" onclick=${update} disabled=${disabled ? disabled : false}>
-        ${label}
+const Button = ({className, label, update, disabled}) => (
+    <button class={className} onclick={update} disabled={disabled ? disabled : false}>
+        {label}
     </button>
-`);
+);
 
-const view = (model, actions) => (html`
+const view = (model, actions) => (
   <div class="counter">
     <h1>Welcome to HyperApp!</h1>
     <hr class="line-break" />
     <section>
-      ${Button({className: 'add', label: '+', update: actions.add})}
-      <h1>${model.num}</h1>
-      ${Button({className: 'sub', label: '-', update: actions.sub, disabled: model.num <= 0})}
-      ${clickCount(model.clicks)}
+      {Button({className: 'add', label: '+', update: actions.add})}
+      <h1>{model.num}</h1>
+      {Button({className: 'sub', label: '-', update: actions.sub, disabled: model.num <= 0})}
+      {clickCount(model.clicks)}
     </section>
   </div>
-`);
+);
 
 export default view;
