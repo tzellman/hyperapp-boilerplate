e95f4570a04b4e4a5bb2289ea2dac543bafc60f8
Tom Zellman <tzellman@gmail.com>
2019-07-06 13:48:57 -0400

    feat: upgdate to latest stable hyperapp + other dep updates

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import './service-worker-registration';
-import {app} from 'hyperapp';
+import { app } from 'hyperapp';
 import actions from './actions/counter';
 import state from './states/counter';
 import view from './views/counter';
